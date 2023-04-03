2697f642c6a7ff8630fa813e2c226a4c85e5831d
Ariful Islam Razu <learnicmind@gmail.com>
2023-04-04 03:51:45 +0600

    color picker

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".btn-refresh");
+
+const maxPaletterBox = 10;
+
+const generatePalette = () => {
+    container.innerHTML = ''; 
+    for (let i = 0; i < maxPaletterBox; i++) {
+
+        // generating a random hex color code
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, "0")}`;
+
+        // creating a new 'li' element and inserting it to the container
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `
+            <div class="react-box" style="background: ${randomHex}"></div>
+            <span class="hex-value">${randomHex}</span>
+
+        `
+        // adding click event to current li element to copy the color
+        color.addEventListener("click", () => copyColor(color, randomHex))
+        container.appendChild(color)
+    }
+}
+generatePalette();
+
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+    // Copying th ehex value, updating the text to copied,
+    // and changing text back to original hex value after 1 second
+    navigator.clipboard.writeText(hexVal).then(() => {
+        colorElement.innerText = "Copied";
+        setTimeout(() => colorElement.innerText = hexVal, 1000)
+    })
+}
+
+refreshBtn.addEventListener("click", generatePalette);
